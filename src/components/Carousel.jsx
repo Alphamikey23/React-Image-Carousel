e221d7a51788afd52c7c4648e35d79ffa0266b87
Jay <eng22cse001@gmail.com>
2024-06-21 17:21:26 +0530

    first commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// implement the class below
-class Carousel extends Component {
-  
-}
+const Carousel = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleNavigation = (direction) => {
+        let newIndex = currentIndex;
+        if (direction === 'prev') {
+            newIndex = (currentIndex - 1 + images.length) % images.length;
+        } else if (direction === 'next') {
+            newIndex = (currentIndex + 1) % images.length;
+        }
+        setCurrentIndex(newIndex);
+    };
+
+    return (
+        <div className="carousel">
+            <div className="image-container">
+                <img src={images[currentIndex].img} alt={`carousel-${currentIndex}`} />
+                <div className="caption">
+                    <h2>{images[currentIndex].title}</h2>
+                    <p>{images[currentIndex].subtitle}</p>
+                </div>
+            </div>
+            <div className="arrow-buttons">
+                <button className="nav-button left" onClick={() => handleNavigation('prev')}>
+                    <ArrowBackIosIcon />
+                </button>
+                <button className="nav-button right" onClick={() => handleNavigation('next')}>
+                    <ArrowForwardIosIcon />
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default Carousel;
